627cb74565ee844b6f9e11da8721fe405c163894
jrosati <brakzog@gmail.com>
2026-06-03 23:39:46 +0200

    add ios target

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.swimtracker"
+    namespace = "com.jro.swimtracker"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.swimtracker"
+        applicationId = "com.jro.swimtracker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
--- android/app/src/main/kotlin/com/example/swimtracker/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/swimtracker/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.swimtracker
+package com.jro.swimtracker
 
 import io.flutter.embedding.android.FlutterActivity
 
