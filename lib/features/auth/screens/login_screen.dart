63563a6686f2d8669f69383d50866e45db4861d5
julien.rosati-extern <julien.rosati-extern@ampere.cars>
2026-06-30 16:21:17 +0200

    d

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:swimtracker/shared/widgets/app_shell.dart';
 import '../../../core/services/auth_service.dart';
 import '../../../shared/theme/app_theme.dart';
 
